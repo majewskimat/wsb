fff8d2d1e4da03708e057feed8b6256b9544bb18
majewskimat <marcogeramia@gmail.com>
2019-10-17 11:30:24 +0200

    Pole prostokąta

---
+++ 1_first_file/Program - pole prostokąta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pole_prostokąta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nProgram obliczający pole prostokąta");
+
+            double a, b, wynik;
+
+            Console.Write("\nPodaj w cm długość boku b: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("\nPodaj w cm długość boku b: ");
+            b = double.Parse(Console.ReadLine());
+
+            wynik = a * b;
+
+            if (a > 0 & b > 0)
+
+            {
+                Console.WriteLine("\nPole prostokąta wynosi: " + wynik + " cm/kw\n");
+
+            }
+
+            else
+            {
+                Console.WriteLine("\nBłędne dane podane z klawiatury\n");          
+            }
+            Console.ReadKey();
+        }
+    }
+}
